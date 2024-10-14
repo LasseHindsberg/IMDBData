7ce8528403f9ba792fc8593ca0a24b2f031b20ab
Fr0styy <lassehindsberg@gmail.com>
2024-10-14 13:10:22 +0200

    made inserter interface

---
+++ IMDBData/IInserter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using IMDBData.Models;
+using System.Data.SqlClient;
+
+namespace IMDBData
+{
+    public interface IInserter
+    {
+       
+        void Insert(List<Title> Titles, List<Person> Persons, List<Crew> Crews, SqlConnection SqlConn, SqlTransaction TransAction);
+    }
+}
